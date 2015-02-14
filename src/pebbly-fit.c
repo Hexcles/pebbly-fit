97a29a681e5d896835e78cae64e6f3333f822ca3
kalryoma <kalryoma@gmail.com>
2015-02-13 19:23:20 -0800

    visual

--- src/pebbly-fit.c
+++ src/pebbly-fit.c
@@ -3,20 +3,21 @@
 #define TS 1
 
 static Window *window;
-//static TextLayer *text_layer;
+static TextLayer *text_layer;
 
 static uint16_t s_worker_steps;
+static char s_text[5];
 
 static void worker_message_handler(uint16_t type, AppWorkerMessage *data) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "s_worker_steps");
   if (type == TS){
     s_worker_steps = data->data0;
     APP_LOG(APP_LOG_LEVEL_DEBUG, "s_worker_steps=%d", s_worker_steps);
-    
-    window_stack_pop_all(false);
+    snprintf(s_text, 5, "%d", s_worker_steps);
+    text_layer_set_text(text_layer, s_text);
+    psleep(1000);
   }
 }
-/**
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
 }
@@ -40,7 +41,7 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text(text_layer, "Pebbly");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
@@ -48,15 +49,15 @@ static void window_load(Window *window) {
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
 }
-**/
+
 static void init(void) {
   window = window_create();
-  /**window_set_click_config_provider(window, click_config_provider);
+  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
-  const bool animated = true;**/
+  const bool animated = true;
   window_stack_push(window, false);
 
   app_worker_message_subscribe(worker_message_handler);
@@ -65,7 +66,7 @@ static void init(void) {
 }
 
 static void deinit(void) {
-  //window_destroy(window);
+  window_destroy(window);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "deinit!");
   app_worker_message_unsubscribe();
 }
